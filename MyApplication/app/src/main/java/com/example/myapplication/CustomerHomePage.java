d117ac2402f1c72271a828e51a52c94ddf49562a
Ryan-Runping <96729973+Ryan-Runping@users.noreply.github.com>
2023-07-28 22:43:30 +0100

    some customer pages updates

--- MyApplication/app/src/main/java/com/example/myapplication/CustomerHomePage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/CustomerHomePage.java
@@ -7,10 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CustomerHomePage extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnCustomerAccount;
+    ImageButton btnCustomerAccount;
 
     @SuppressLint("MissingInflatedId")
     @Override
