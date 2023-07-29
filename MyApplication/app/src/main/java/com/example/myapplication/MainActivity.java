7359a8572384fe9fa3162cd869c5d698c903f73a
Ryan-Runping <96729973+Ryan-Runping@users.noreply.github.com>
2023-07-29 17:30:35 +0100

    update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.myapplication.frontendCustomer.CustomerLogin;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnCustomer;
-    Button btnProvider;
+    ImageButton btnCustomer;
+    ImageButton btnProvider;
 
 
     @Override
