98568032f97e936c918000ec2c79ebb10e5ba584
Ryan-Runping <96729973+Ryan-Runping@users.noreply.github.com>
2023-08-08 09:59:18 +0100

    update account security page , now it's in one page( switch mode by edit button)

--- MyApplication/app/src/main/java/com/example/myapplication/frontendCustomer/AccountPage/CustomerSettingAccountSecurity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/frontendCustomer/AccountPage/CustomerSettingAccountSecurity.java
@@ -1,21 +1,18 @@
 package com.example.myapplication.frontendCustomer.AccountPage;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -30,12 +27,14 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subscribers.ResourceSubscriber;
 
-public class CustomerSettingAccountSecurity extends AppCompatActivity implements View.OnClickListener {
+public class CustomerSettingAccountSecurity extends AppCompatActivity {
 
-      ImageButton AS_username;
-      ImageButton AS_phone;
-      ImageButton AS_email;
-      ImageButton As_password;
+
+    Button edit;
+    Button save;
+    LinearLayout acLayout;
+    boolean isEditMode = false;
+
 
 
     @Override
@@ -44,24 +43,70 @@ public class CustomerSettingAccountSecurity extends AppCompatActivity implements
         setContentView(R.layout.activity_customer_setting_account_seurity);
 
         setToolBar();
-        initializeButton();
+
+        edit = findViewById(R.id.edit);
+        acLayout =findViewById(R.id.acLinearLayout);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchMode();
+            }
+        });
+
+
     }
 
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.AS_username) {
-//            alertDialog();
-            startActivity(new Intent(CustomerSettingAccountSecurity.this, CustomerResetUsername.class));
-        } else if (view.getId() == R.id.AS_phone) {
-            startActivity(new Intent(CustomerSettingAccountSecurity.this, CustomerResetPhone.class));
-        }else if (view.getId() == R.id.AS_email) {
-            startActivity(new Intent(CustomerSettingAccountSecurity.this, CustomerResetEmail.class));
-        }else if (view.getId() == R.id.AS_password) {
-            startActivity(new Intent(CustomerSettingAccountSecurity.this, CustomerResetPassword.class));
+    void switchMode() {
+        isEditMode = !isEditMode;
+
+        if (!isEditMode){
+            edit.setText("Edit");
+            //update database should be here
+        }else {
+            edit.setText("Save");
         }
 
+        for (int i = 0; i < acLayout.getChildCount(); i++) {
+            View child = acLayout.getChildAt(i);
+            if (child instanceof TextView && child != save && child != edit) {
+                TextView textView = (TextView) child;
+                if (isEditMode) {
+                    EditText editText = new EditText(this);
+                    editText.setLayoutParams(textView.getLayoutParams());
+                    editText.setText(textView.getText());
+                    editText.setBackground(textView.getBackground());
+                    float textSizeInPixels = textView.getTextSize();
+                    float textSizeInSp = textSizeInPixels / getResources().getDisplayMetrics().scaledDensity;
+                    editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSizeInSp);
+                    editText.setElevation(textView.getElevation());
+                    acLayout.removeView(textView);
+                    acLayout.addView(editText, i);
+                } else {
+                    EditText editText = (EditText) child;
+                    TextView newTextView = new TextView(this);
+                    newTextView.setLayoutParams(editText.getLayoutParams());
+                    newTextView.setText(editText.getText());
+                    float paddingDp = 15;  // 15dp
+                    int paddingPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, paddingDp, getResources().getDisplayMetrics());
+                    newTextView.setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
+                    applyTextViewStyle(newTextView, editText); // Apply original style
+                    acLayout.removeView(editText);
+                    acLayout.addView(newTextView, i);
+                }
+                }
+            }
+        }
+
+
+    private void applyTextViewStyle(TextView target, TextView source) {
+        target.setBackground(source.getBackground());
+        target.setTextColor(source.getCurrentTextColor());
+        target.setTextSize(TypedValue.COMPLEX_UNIT_PX, source.getTextSize());
+        target.setElevation(source.getElevation());
     }
+
 
 
 
@@ -76,18 +121,6 @@ public class CustomerSettingAccountSecurity extends AppCompatActivity implements
         });
     }
 
-    private void initializeButton() {
-        AS_username = findViewById(R.id.AS_username);
-        AS_phone = findViewById(R.id.AS_phone);
-        AS_email = findViewById(R.id.AS_email);
-        As_password = findViewById(R.id.AS_password);
-
-        AS_username.setOnClickListener(this);
-        AS_phone.setOnClickListener(this);
-        AS_email.setOnClickListener(this);
-        As_password.setOnClickListener(this);
-
-    }
 
     //For future use.
     @SuppressLint("CheckResult")
