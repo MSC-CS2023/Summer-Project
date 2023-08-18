76d9ec6042247f78e98311133d0f65fc44ec5b1e
Ryan-Runping <96729973+Ryan-Runping@users.noreply.github.com>
2023-08-18 23:29:52 +0100

    update timetable

--- MyApplication/app/src/main/java/com/example/myapplication/frontendCustomer/AccountPage/CustomerTimetablePage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/frontendCustomer/AccountPage/CustomerTimetablePage.java
@@ -1,16 +1,30 @@
 package com.example.myapplication.frontendCustomer.AccountPage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 
 public class CustomerTimetablePage extends AppCompatActivity {
 
+    CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_timetable_page);
+
+        calendarView = findViewById(R.id.calender);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(CustomerTimetablePage.this, "you chose" + year + "/" + (month+1) + "/" + dayOfMonth, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
